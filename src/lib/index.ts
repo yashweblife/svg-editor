ab290c6f5ad923910e8e16602a09ac3cbd29113d
yashweblife <yashweblife20@gmail.com>
2025-06-18 15:20:41 -0500

    add distance formula

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,6 +2,10 @@ import { action_types, basic_object, tool_types } from "../types";
 import Canvas, { c, canvas, canvas_background, grid_dots, helper_line_color } from "./Canvas";
 import EventMap from "./Events";
 import mouse from "./Mouse";
+
+function distance(a:{x: number, y: number}, b:{x: number, y: number}){
+    return Math.sqrt((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y));
+}
 const settings:{
     current_tool: tool_types,
     current_action: action_types,
@@ -12,5 +16,5 @@ const settings:{
     current_object: null
 }
 
-export { c, Canvas, canvas, canvas_background, EventMap, grid_dots, helper_line_color, mouse, settings };
+export { c, Canvas, canvas, canvas_background, distance, EventMap, grid_dots, helper_line_color, mouse, settings };
 
