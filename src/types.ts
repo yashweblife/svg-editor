057ac09a45b2cb39ac24de3ee84bcc9391cdecbc
yashweblife <yashweblife20@gmail.com>
2025-06-18 14:54:17 -0500

    add paths to b_o

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,3 @@
 export type action_types =  "draw"|"none"|"edit"
-export type tool_types = "circle"| "line"| "rectangle"|"none" 
-export type basic_object = {x: number, y: number, rx: number, ry: number, type: tool_types}
+export type tool_types = "circle"| "line"| "rectangle"|"path"|"none" 
+export type basic_object = {x: number, y: number, rx: number, ry: number,paths?:{x: number, y: number}[], type: tool_types}
