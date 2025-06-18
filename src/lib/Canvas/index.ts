9f3c23a24a0f0266cea078579ee4dab62a8c86c7
yashweblife <yashweblife20@gmail.com>
2025-06-18 11:44:48 -0500

    add to parent

--- src/lib/Canvas/index.ts
+++ src/lib/Canvas/index.ts
@@ -4,5 +4,8 @@ export default class Canvas {
     constructor() {
         this.canvas = document.createElement("canvas") as HTMLCanvasElement;
         this.c = this.canvas.getContext("2d") as CanvasRenderingContext2D;
+        this.canvas.width = window.innerWidth;
+        this.canvas.height = window.innerHeight;
+        (document.querySelector("#app") as HTMLDivElement).appendChild(this.canvas);
     }
 }
