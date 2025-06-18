28625306acfda7b7e5c42be8ff2d6530ae0ba831
yashweblife <yashweblife20@gmail.com>
2025-06-18 14:54:53 -0500

    add pipe exports

---
+++ src/lib/index.ts
@@ -0,0 +1,16 @@
+import { action_types, basic_object, tool_types } from "../types";
+import Canvas, { c, canvas, canvas_background } from "./Canvas";
+import EventMap from "./Events";
+import mouse from "./Mouse";
+const settings:{
+    current_tool: tool_types,
+    current_action: action_types,
+    current_object: null | basic_object
+} = {
+    current_tool: "circle",
+    current_action: "none",
+    current_object: null
+}
+
+export { c, Canvas, canvas, canvas_background, EventMap, mouse, settings };
+
