8e195da053e1fb436f7d9420a868d02a01e8cf7b
yashweblife <yashweblife20@gmail.com>
2025-06-18 14:35:09 -0500

    initial work for guides

--- src/main.ts
+++ src/main.ts
@@ -51,12 +51,16 @@ function handleEscapeKey(e: KeyboardEvent) {
   }
 }
 function handleHotKeyDraw(e: KeyboardEvent) {
-  e.preventDefault();
+  // e.preventDefault();
   let isValidKey: tool_types = "none"
   if (e.key === "c") {
     isValidKey = "circle"
   } else if (e.key === "r") {
     isValidKey = "rectangle"
+  } else if (e.key === "l") {
+    isValidKey = "line"
+  } else if (e.key === "p") {
+    isValidKey = "path"
   }
   if (isValidKey !== "none") {
     current_tool = isValidKey;
@@ -72,21 +76,58 @@ function handleSelectCircle() {
 }
 function handleCanvasClick(e: MouseEvent) {
   if (current_action === "draw" && current_tool !== "none") {
-    if (current_object == null) {
-      current_object = {
-        x: e.offsetX,
-        y: e.offsetY,
-        rx: 0,
-        ry: 0,
-        type: current_tool
+    if (current_tool === "path") {
+      console.log("path")
+      if (current_object == null) {
+        current_object = {
+          x: 0,
+          y: 0,
+          rx: 0,
+          ry: 0,
+          paths: [{ x: e.offsetX, y: e.offsetY }],
+          type: current_tool
+        }
+        console.log("ADDED:", current_object)
+        return
       }
+      if (current_object.paths) {
+        let pointB = { x: e.offsetX, y: e.offsetY };
+        if (current_object.paths.length > 1) {
+          let pointA = current_object.paths[0];
+          let distance = Math.sqrt((pointB.x - pointA.x) * (pointB.x - pointA.x) + (pointB.y - pointA.y) * (pointB.y - pointA.y));
+          if (distance < 10) {
+            console.log("FINISHING: ", current_object.paths)
+            current_object.paths.push(pointA);
+            objects.push(current_object);
+            current_object = null;
+            current_action = "none";
+            current_tool = "none";
+            return;
+          }
+          current_object.paths.push(pointB);
+        } else {
+          console.log("CONTINUING: ", current_object.paths)
+          current_object.paths.push(pointB);
+        }
+      }
+
     } else {
-      current_object.rx = e.offsetX;
-      current_object.ry = e.offsetY;
-      objects.push(current_object);
-      current_object = null;
-      current_action = "none";
-      current_tool = "none";
+      if (current_object == null) {
+        current_object = {
+          x: e.offsetX,
+          y: e.offsetY,
+          rx: 0,
+          ry: 0,
+          type: current_tool
+        }
+      } else {
+        current_object.rx = e.offsetX;
+        current_object.ry = e.offsetY;
+        objects.push(current_object);
+        current_object = null;
+        current_action = "none";
+        current_tool = "none";
+      }
     }
   }
 }
@@ -105,19 +146,64 @@ function animation() {
       c.closePath();
     }
   }
+  if (current_action === "draw" && objects.length > 0) {
+    
+      let closest_object = objects[0];
+      let closest_distance = Math.sqrt((mouse.x - closest_object.x) * (mouse.x - closest_object.x) + (mouse.y - closest_object.y) * (mouse.y - closest_object.y));
+      for (let i = 0; i < objects.length; i++) {
+        let b = { x: objects[i].x, y: objects[i].y, rx: objects[i].rx, ry: objects[i].ry };
+        let distance1 = Math.sqrt((mouse.x - b.x) * (mouse.x - b.x) + (mouse.y - b.y) * (mouse.y - b.y));
+        if (distance1 < closest_distance) {
+          closest_distance = distance1;
+          closest_object = objects[i];
+        }
+      }
+      c.beginPath();
+      c.arc(closest_object.x, closest_object.y, 2, 0, 2 * Math.PI);
+      c.rect(closest_object.x, closest_object.y, mouse.x - closest_object.x, mouse.y - closest_object.y);
+      c.strokeStyle = "rgba(166, 105, 105, 0.68)";
+      c.stroke();
+      c.closePath();
+      c.strokeStyle = "rgba(255,255,255,0.1)";
+    
+  }
   if (current_action === "draw" && current_object != null) {
-    if( current_tool === "circle"){
+    if (current_tool === "circle") {
       const dist = Math.sqrt((mouse.x - current_object.x) ** 2 + (mouse.y - current_object.y) ** 2);
       c.beginPath();
       c.arc(current_object.x, current_object.y, dist, 0, 2 * Math.PI);
       c.stroke();
       c.closePath();
-    }else if( current_tool === "rectangle"){
+    } else if (current_tool === "rectangle") {
       c.beginPath();
       c.rect(current_object.x, current_object.y, mouse.x - current_object.x, mouse.y - current_object.y);
       c.stroke();
       c.closePath();
-    }   
+    } else if (current_tool === "line") {
+      c.beginPath();
+      c.moveTo(current_object.x, current_object.y);
+      c.lineTo(mouse.x, mouse.y);
+      c.stroke();
+      c.closePath();
+    } else if (current_tool === "path") {
+      if (current_object.paths) {
+        let prev = current_object.paths[0];
+        for (let i = 1; i < current_object.paths.length; i++) {
+          c.beginPath();
+          c.moveTo(prev.x, prev.y);
+          c.lineTo(current_object.paths[i].x, current_object.paths[i].y);
+          prev = current_object.paths[i];
+          c.stroke();
+          c.closePath();
+        }
+        c.beginPath();
+        let p = current_object.paths[current_object.paths.length - 1]
+        c.moveTo(p.x, p.y);
+        c.lineTo(mouse.x, mouse.y);
+        c.stroke();
+        c.closePath();
+      }
+    }
   }
   for (let i = 0; i < objects.length; i++) {
     c.beginPath();
@@ -127,9 +213,28 @@ function animation() {
       c.arc(o.x, o.y, dist, 0, 2 * Math.PI);
       c.stroke();
     }
-    else if(objects[i].type === "rectangle"){
+    else if (objects[i].type === "rectangle") {
       c.rect(o.x, o.y, o.rx - o.x, o.ry - o.y);
-      c.stroke();  
+      c.stroke();
+    }
+    else if (objects[i].type === "line") {
+      c.moveTo(o.x, o.y);
+      c.lineTo(o.rx, o.ry);
+      c.stroke();
+    }
+    else if (objects[i].type === "path") {
+      c.beginPath();
+      const paths = o.paths;
+      if (!paths || paths.length === 0) {
+        continue;
+      }
+      let prev = paths[0];
+      for (let i = 1; i < paths.length; i++) {
+        c.moveTo(prev.x, prev.y);
+        c.lineTo(paths[i].x, paths[i].y);
+        prev = paths[i];
+      }
+      c.stroke();
     }
     c.closePath();
   }
