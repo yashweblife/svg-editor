5f81ac24174a5e243a093f53dc2f86b8f3e2e90a
yashweblife <yashweblife20@gmail.com>
2025-06-18 13:24:26 -0500

    new type

--- src/types.ts
+++ src/types.ts
@@ -1,2 +1,3 @@
 export type action_types =  "draw"|"none"|"edit"
 export type tool_types = "circle"| "line"| "rectangle"|"none" 
+export type basic_object = {x: number, y: number, rx: number, ry: number, type: tool_types}
