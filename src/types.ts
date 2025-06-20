92919a74b568f37828ef5dae201e667c28de9781
yashweblife <yashweblife20@gmail.com>
2025-06-19 23:26:39 -0500

    new props

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,4 @@
 export type action_types =  "draw"|"none"|"edit"
 export type tool_types = "circle"| "line"| "rectangle"|"path"|"none" 
 export type vec2d = {x: number, y: number}
-export type basic_object = {x: number, y: number, rx: number, ry: number,paths?: vec2d[], type: tool_types}
+export type basic_object = {x: number, y: number, rx: number, ry: number,paths?: vec2d[], type: tool_types,r?:number}
