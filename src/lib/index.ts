93678a48541d22d5cb829c533e8087ea90d065e5
yashweblife <yashweblife20@gmail.com>
2025-06-23 12:15:30 -0500

    add sticky point

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,9 +1,9 @@
-import { action_types, basic_object, tool_types } from "../types";
+import { action_types, basic_object, tool_types, vec2d } from "../types";
 import Canvas, { c, canvas, canvas_background, grid_dots, helper_line_color } from "./Canvas";
 import EventMap from "./Events";
 import mouse from "./Mouse";
 
-function distance(a:{x: number, y: number}, b:{x: number, y: number}){
+function distance(a:vec2d, b:vec2d){
     return Math.sqrt((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y));
 }
 const settings:{
@@ -12,7 +12,8 @@ const settings:{
     current_object: null | basic_object,
     objects: basic_object[],
     selected_object: null | basic_object,
-    closest_object: null | basic_object
+    closest_object: null | basic_object,
+    sticky_point: null | vec2d,
     
 } = {
     current_tool: "circle",
@@ -20,7 +21,8 @@ const settings:{
     current_object: null,
     objects: [],
     selected_object: null,
-    closest_object: null
+    closest_object: null,
+    sticky_point: null
 }
 
 export { c, Canvas, canvas, canvas_background, distance, EventMap, grid_dots, helper_line_color, mouse, settings };
