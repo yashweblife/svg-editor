e963791200528b1fce21d1e494b1313a1a0c8b2d
yashweblife <yashweblife20@gmail.com>
2025-06-23 13:09:05 -0500

    export helpers

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,6 +2,7 @@ import { action_types, basic_object, tool_types, vec2d } from "../types";
 import Canvas, { c, canvas, canvas_background, grid_dots, helper_line_color } from "./Canvas";
 import EventMap from "./Events";
 import mouse from "./Mouse";
+import { Arc, Line, Rect } from "./Vector";
 
 function distance(a:vec2d, b:vec2d){
     return Math.sqrt((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y));
@@ -27,5 +28,5 @@ const settings:{
     canvas_center: { x: canvas.width / 2, y: canvas.height / 2 }
 }
 
-export { c, Canvas, canvas, canvas_background, distance, EventMap, grid_dots, helper_line_color, mouse, settings };
+export { Arc, c, Canvas, canvas, canvas_background, distance, EventMap, grid_dots, helper_line_color, Line, mouse, Rect, settings };
 
