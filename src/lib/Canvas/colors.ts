48722b356d59a78334078a9233d44375a435044e
yashweblife <yashweblife20@gmail.com>
2025-06-25 10:31:13 -0500

    new file for colors

---
+++ src/lib/Canvas/colors.ts
@@ -0,0 +1,9 @@
+const canvas_background = "rgb(18,18,18)";
+const grid_dots = "rgba(255,255,255,0.1)";
+const helper_line_color = "rgba(166, 105, 105, 0.68)";
+
+export {
+    canvas_background,
+    grid_dots,
+    helper_line_color
+};
