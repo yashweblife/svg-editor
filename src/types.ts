64a979c029d945c6f66540419edca7306784b823
yashweblife <yashweblife20@gmail.com>
2025-06-24 21:21:09 -0500

    format

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,4 @@
-export type action_types =  "draw"|"none"|"edit"
-export type tool_types = "circle"| "line"| "rectangle"|"path"|"none" 
-export type vec2d = {x: number, y: number}
-export type basic_object = {rx: number, ry: number,paths?: vec2d[], type: tool_types,r?:number} &vec2d
+export type action_types = "draw" | "none" | "edit"
+export type tool_types = "circle" | "line" | "rectangle" | "path" | "none"
+export type vec2d = { x: number, y: number }
+export type basic_object = { rx: number, ry: number, paths?: vec2d[], type: tool_types, r?: number } & vec2d
