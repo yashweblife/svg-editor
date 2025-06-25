670c5825ff362fa286273d08303cb34a2e42a2fc
yashweblife <yashweblife20@gmail.com>
2025-06-24 21:20:07 -0500

    format

--- src/lib/Events/index.ts
+++ src/lib/Events/index.ts
@@ -1,6 +1,6 @@
-export default function EventMap<T extends Event>(parent: Window|HTMLElement, event:string, callback:((e:T) => void)[]){
-    const listener = parent.addEventListener(event, (e:Event) => {
-        for(let i=0; i<callback.length; i++){
+export default function EventMap<T extends Event>(parent: Window | HTMLElement, event: string, callback: ((e: T) => void)[]) {
+    const listener = parent.addEventListener(event, (e: Event) => {
+        for (let i = 0; i < callback.length; i++) {
             callback[i](e as T);
         }
     })
