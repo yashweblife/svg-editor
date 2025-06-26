c10cd0221a8737595fb5ba73fa274f7bd3d7ca54
yashweblife <yashweblife20@gmail.com>
2025-06-26 10:17:17 -0500

    format

--- src/main.ts
+++ src/main.ts
@@ -5,23 +5,23 @@ import { resizeWindow } from "./lib/Events/handlers";
 import "./styles/common.css";
 
 const circleButton = document.querySelector("#circle") as HTMLButtonElement;
-const {c, canvas} = Canvas.makeCanvas("#app");
-const settings = {...s}
-EventMap<MouseEvent>(circleButton, "click", [()=>{handleSelectCircle(settings)}]);
-EventMap<MouseEvent>(canvas, "click", [(e: MouseEvent)=>{handleCanvasClick(e, settings)}]);
-EventMap<Event>(window, "resize", [()=>{resizeWindow(canvas)}]);
-EventMap<KeyboardEvent>(window, "keydown", [(e: KeyboardEvent)=>{handleEscapeKey(e, settings)}, (e: KeyboardEvent)=>{handleHotKeyDraw(e,settings);}]);
-EventMap<MouseEvent>(canvas, "mousemove", [(e: MouseEvent)=>{handleCanvasMouseMove(e, mouse,settings)}, (e: MouseEvent)=>{handleDragObject(e, mouse, settings)}]);
-EventMap<MouseEvent>(canvas, "mousedown", [(e: MouseEvent)=>{handleCanvasMouseDown(e, mouse)}]);
-EventMap<MouseEvent>(canvas, "mouseup", [(e: MouseEvent)=>{handleCanvasMouseUp(e, mouse)}]);
+const { c, canvas } = Canvas.makeCanvas("#app");
+const settings = { ...s }
+EventMap<MouseEvent>(circleButton, "click", [() => { handleSelectCircle(settings) }]);
+EventMap<MouseEvent>(canvas, "click", [(e: MouseEvent) => { handleCanvasClick(e, settings) }]);
+EventMap<Event>(window, "resize", [() => { resizeWindow(canvas) }]);
+EventMap<KeyboardEvent>(window, "keydown", [(e: KeyboardEvent) => { handleEscapeKey(e, settings) }, (e: KeyboardEvent) => { handleHotKeyDraw(e, settings); }]);
+EventMap<MouseEvent>(canvas, "mousemove", [(e: MouseEvent) => { handleCanvasMouseMove(e, mouse, settings) }, (e: MouseEvent) => { handleDragObject(e, mouse, settings) }]);
+EventMap<MouseEvent>(canvas, "mousedown", [(e: MouseEvent) => { handleCanvasMouseDown(e, mouse) }]);
+EventMap<MouseEvent>(canvas, "mouseup", [(e: MouseEvent) => { handleCanvasMouseUp(e, mouse) }]);
 
 
 function animation() {
   requestAnimationFrame(animation);
   clearCanvas(c);
   drawGrid(c);
-  drawMouseTool(c,mouse, settings);
-  drawHelperGuides(c,mouse, settings);
+  drawMouseTool(c, mouse, settings);
+  drawHelperGuides(c, mouse, settings);
   drawPhantomObject(c, mouse, settings);
   drawObjects(c, mouse, settings);
 }
