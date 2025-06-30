36badd5e6e679f0832cea7d898aa2c16209cf797
yashweblife <yashweblife20@gmail.com>
2025-06-29 19:36:33 -0500

    feature:add pan to camera

--- src/lib/Camera/index.ts
+++ src/lib/Camera/index.ts
@@ -1,4 +1,8 @@
 export default class Camera {
     public zoom = 0;
     constructor(public x: number, public y: number) { }
+    pan(x: number, y: number) {
+        this.x += x;
+        this.y += y;
+    }
 }
