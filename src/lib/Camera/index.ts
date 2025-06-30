0ad1bb9dcff899dc0e11e675757a8a8fec54b315
yashweblife <yashweblife20@gmail.com>
2025-06-29 19:37:40 -0500

    feature: apply translation

--- src/lib/Camera/index.ts
+++ src/lib/Camera/index.ts
@@ -1,8 +1,14 @@
+import { basic_object } from "../../types";
+
 export default class Camera {
     public zoom = 0;
     constructor(public x: number, public y: number) { }
     pan(x: number, y: number) {
         this.x += x;
         this.y += y;
+    }
+    applyTranslation(obj:basic_object){
+        obj.x += this.x;
+        obj.y += this.y;
     }
 }
