fb604795f5daf20bf273656fe0257f833e68c1f8
yashweblife <yashweblife20@gmail.com>
2025-07-01 09:52:07 -0500

    default value

--- src/lib/Camera/index.ts
+++ src/lib/Camera/index.ts
@@ -2,7 +2,7 @@ import { basic_object } from "../../types";
 
 export default class Camera {
     public zoom = 0;
-    constructor(public x: number, public y: number) { }
+    constructor(public x: number = window.innerWidth / 2, public y: number = window.innerHeight / 2) { }
     pan(x: number, y: number) {
         this.x += x;
         this.y += y;
