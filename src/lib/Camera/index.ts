1d99fa62ad88a8b17c5870c4e4d02a8925f15acd
yashweblife <yashweblife20@gmail.com>
2025-07-01 09:59:45 -0500

    manage zoom

--- src/lib/Camera/index.ts
+++ src/lib/Camera/index.ts
@@ -1,14 +1,19 @@
 import { basic_object } from "../../types";
 
 export default class Camera {
-    public zoom = 0;
     constructor(public x: number = window.innerWidth / 2, public y: number = window.innerHeight / 2) { }
     pan(x: number, y: number) {
         this.x += x;
         this.y += y;
     }
-    applyTranslation(obj:basic_object){
+    applyTranslation(obj: basic_object) {
         obj.x += this.x;
         obj.y += this.y;
+    }
+    set zoom(val: number) {
+        this.zoom = val > 100 ? 100 : val < 0 ? 0 : val
+    }
+    get zoom(): number {
+        return this.zoom
     }
 }
