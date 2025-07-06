efea4344f8882939038277d81dc37760bb6108e2
yashweblife <yashweblife20@gmail.com>
2025-07-05 19:47:39 -0500

    format

--- src/lib/Camera/index.ts
+++ src/lib/Camera/index.ts
@@ -1,7 +1,10 @@
 import { basic_object } from "../../types";
 
 export default class Camera {
-    constructor(public x: number = window.innerWidth / 2, public y: number = window.innerHeight / 2) { }
+    constructor(
+        public x: number = window.innerWidth / 2, 
+        public y: number = window.innerHeight / 2
+    ) { }
     pan(x: number, y: number) {
         this.x += x;
         this.y += y;
