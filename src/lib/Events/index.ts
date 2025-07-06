a1f8dc763709bb248e998bc5ea6062cfb3d2012c
yashweblife <yashweblife20@gmail.com>
2025-07-05 19:53:56 -0500

    format

--- src/lib/Events/index.ts
+++ src/lib/Events/index.ts
@@ -9,12 +9,20 @@ import {
     handleSelectCircle
 } from "./handlers";
 
-export default function EventMap<T extends Event>(parent: Window | HTMLElement, event: string, callback: ((e: T) => void)[]) {
-    const listener = parent.addEventListener(event, (e: Event) => {
-        for (let i = 0; i < callback.length; i++) {
-            callback[i](e as T);
-        }
-    })
+export default function EventMap<T extends Event>(
+    parent: Window | HTMLElement,
+    event: string,
+    callback: ((e: T) => void)[]
+) {
+    const listener = parent
+        .addEventListener(
+            event,
+            (e: Event) => {
+                for (let i = 0; i < callback.length; i++) {
+                    callback[i](e as T);
+                }
+            }
+        )
     return listener
 }
 
